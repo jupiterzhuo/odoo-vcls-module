ff0490cf15c1c023c13f529daec39b8a299b4dca
MarwanLaam <laamouri@voisinconsulting.com>
2020-05-25 11:56:16 +0200

    Update __manifest__.py

--- vcls-etl-access/__manifest__.py
+++ vcls-etl-access/__manifest__.py
@@ -15,14 +15,15 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.0.0',
+    'version': '1.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
                 'vcls-contact',
                 'vcls-hr',
-                'vcls-suppliers',
-                'vcls-rdd',
+                'vcls-project',
+                'vcls-timesheet',
+                'vcls-expenses',
                 ],
 
     # always loaded
