3b26a9ed01dd4bc505f745eb26f9cc056c1f56f0
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-05-27 09:49:27 +0200

    FIX subscription

--- vcls_subscription/__manifest__.py
+++ vcls_subscription/__manifest__.py
@@ -11,7 +11,7 @@
     'author': "VCLS",
     'website': "http://www.voisinconsulting.com",
     'category': 'Uncategorized',
-    'version': '0.0.3',
+    'version': '0.0.4',
     'depends': [
         'base',
         'sale_subscription',
