ad199446c8ef292ef00806a5d7d94bde1a6f942b
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-05-27 09:35:48 +0200

    Update sale_subscription.py

--- vcls_subscription/models/sale_subscription.py
+++ vcls_subscription/models/sale_subscription.py
@@ -76,8 +76,9 @@ class SaleSubscription(models.Model):
                             so_line = found
                         _logger.info("SUB | Adding {} on {} for {} in {}".format(line.quantity,so_line.qty_delivered,so_line.name,so_line.order_id.name))
                         _logger.info("SUB | Manual vs  Delivered Before {} {}".format(so_line.qty_delivered_manual,so_line.qty_delivered))
-                        so_line.qty_delivered_manual += line.quantity
-                        so_line.qty_delivered = so_line.qty_delivered_manual
+                        so_line.qty_delivered += line.quantity
+                        #so_line.qty_delivered = so_line.qty_delivered_manual
+                        so_line._inverse_qty_delivered() #we mimic the manual change of the delivered qty by calling the onchange method
                         _logger.info("SUB | Manual vs  Delivered After {} {}".format(so_line.qty_delivered_manual,so_line.qty_delivered))
 
                 next_date = sub.recurring_next_date or current_date
