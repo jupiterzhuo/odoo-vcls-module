1d3dc437dc4b8f61b2fd23c519d21bc2cf9dec14
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-06-02 15:18:08 +0200

    Update translation cleaner

--- vcls-theme/models/ir_translation.py
+++ vcls-theme/models/ir_translation.py
@@ -12,6 +12,16 @@ class Translation(models.Model):
     @api.model
     def force_name_to_uk(self):
         to_update = self.search([('name','like',',name'),('lang','=','en_GB'),('res_id','!=',0)])
+        count = 0
         for item in to_update:
+            count += 1
             if item.source != item.value: #we find the case when the name has been updated and not the source
-                _logger.info("TRANS to update {}<{}".format(item.source,item.value))
+                force_val = item.value
+                #we get other related translations
+                to_force = self.search([('name','=',item.name),('res_id','=',item.res_id)])
+                _logger.info("TRANS update {}/{} | {} {} > {}".format(count, len(to_update),to_force.mapped('source'),to_force.mapped('value'),item.value))
+                to_force.write({
+                    'source':item.value,
+                    'value':item.value,
+                })
+                
