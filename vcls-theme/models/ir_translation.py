16dfb0e9953dfb3775c005d41a40fb23e209e572
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-06-02 15:26:31 +0200

    Update ir_translation.py

--- vcls-theme/models/ir_translation.py
+++ vcls-theme/models/ir_translation.py
@@ -19,7 +19,7 @@ class Translation(models.Model):
                 force_val = item.value
                 #we get other related translations
                 to_force = self.search([('name','=',item.name),('res_id','=',item.res_id)])
-                _logger.info("TRANS update {}/{} | {} {} > {}".format(count, len(to_update),to_force.mapped('source'),to_force.mapped('value'),item.value))
+                _logger.info("TRANS update {}/{} | {} {} {} > {} {}".format(count, len(to_update),to_force.mapped('res_id'),to_force.mapped('source'),to_force.mapped('value'),item.res_id,item.value))
                 to_force.write({
                     'source':item.value,
                     'value':item.value,
