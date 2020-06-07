4b222c3c0683b26da3b6fe239b921473bb58d87d
Keaton <k.aylor@voisinconsulting.com>
2020-06-07 17:32:54 +0200

    Yooz removal and PO updates
    added onchanges to connect the PO lines together

--- vcls-suppliers/models/purchase.py
+++ vcls-suppliers/models/purchase.py
@@ -1,8 +1,95 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
-from odoo.exceptions import UserError, ValidationError
+
+class AccountAccount(models.Model):
+    _inherit = 'account.account'
+
+    approver_id = fields.Many2one(
+        'res.users',
+        string='Approver'
+        )
+
+
+class Invoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.onchange('partner_id')
+    def _part_change(self):
+        inv_type = self.type or self.env.context.get('type', 'out_invoice')
+
+        if inv_type in ('in_invoice', 'in_refund'):
+            if self.partner_id:
+                if self.company_id != self.partner_id.company_id:
+                    self.company_id = self.partner_id.company_id
+
+    @api.onchange('company_id')
+    def vcls_onchange(self):
+        if not self.env.context.get('journal_id') and self.partner_id and self.type in ['in_invoice', 'in_refund']:
+            journal_domain = [
+                ('type', '=', 'purchase'),
+                ('company_id', '=', self.company_id.id),
+            ]
+            default_journal_id = self.env['account.journal'].search(journal_domain, limit=1)
+            if default_journal_id:
+                self.journal_id = default_journal_id
+        else:
+            self.journal_id = self.env.context.get('journal_id')
+
+        for invoice_line in self.invoice_line_ids:
+            invoice_line = invoice_line.with_context(
+                default_company_id=self.company_id.id,
+                force_company=self.company_id.id
+            )
+            fpos = self.fiscal_position_id
+            company = self.company_id
+            type = self.type
+            product = invoice_line.product_id
+
+            account = invoice_line.get_invoice_line_account(type, product, fpos, company)
+            if account:
+                invoice_line.account_id = account.id
+            else:
+                invoice_line.account_id = False
+
+            invoice_line._set_taxes()
+
+    @api.onchange('journal_id')
+    def _get_company_currency(self):
+        invoice_type = self.type or self.env.context.get('type', 'out_invoice')
+        if invoice_type in ('in_invoice', 'in_refund'):
+            if self.journal_id and self.journal_id.company_id.currency_id:
+                self.currency_id = self.partner_id.currency_id or self.journal_id.currency_id.id or self.journal_id.company_id.currency_id.id
+
+    @api.multi
+    def action_purchase_approval(self):
+        for invoice in self:
+            activity_type = self.env['mail.activity.type'].search([('name', '=', 'Invoice Review')], limit=1)
+            if activity_type:
+                users_to_notify = self.env['res.users']
+                for invoice_line in invoice.invoice_line_ids:
+                    if invoice_line.account_analytic_id:
+                        project = self.env['project.project'].search([('analytic_account_id', '=', invoice_line.account_analytic_id.id)], limit=1)
+                        if project and project.user_id:
+                            users_to_notify |= project.user_id
+                    if invoice_line.account_id.approver_id:
+                        users_to_notify |= invoice_line.account_id.approver_id
+                if not users_to_notify:
+                    raise UserError('No one is eligible to approve this')
+                    return
+                invoice.write({'ready_for_approval': True})
+                for user in users_to_notify:
+                    self.env['mail.activity'].create({
+                        'res_id': invoice.id,
+                        'res_model_id': self.env.ref('account.model_account_invoice').id,
+                        'activity_type_id': activity_type.id,
+                        'user_id': user.id,
+                        'summary': ('Please review the invoice PDF for {}.').format(
+                            invoice.name),
+                    })
+
 
 class PurchaseOrder(models.Model):
 
@@ -73,5 +160,3 @@ class PurchaseOrder(models.Model):
             if user.has_group('vcls-suppliers.vcls_group_rm'):
                 rec.access_level = 'rm'
                 continue
-    
-    
