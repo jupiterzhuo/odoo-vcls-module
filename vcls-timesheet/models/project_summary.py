b963c72530056ab57f46b088b33256b156ff36c0
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-06-17 08:50:40 +0200

    FIXING project Summary

--- vcls-timesheet/models/project_summary.py
+++ vcls-timesheet/models/project_summary.py
@@ -10,10 +10,11 @@ _logger = logging.getLogger(__name__)
 class ProjectSummary(models.Model):
     _inherit = 'project.summary'
 
-    valuation_ratio = fields.Float(compute='compute_valuation_ratio', string='Valuation Ratio', store=True)
+    valuation_ratio = fields.Float(string='Valuation Ratio')
+    #valuation_ratio = fields.Float(compute='compute_valuation_ratio', string='Valuation Ratio', store=True)
 
-    @api.multi
+    """ @api.multi
     @api.depends("project_id.valuation_ratio")
     def compute_valuation_ratio(self):
         for summary in self:
-            summary.valuation_ratio = summary.project_id.valuation_ratio
+            summary.valuation_ratio = summary.project_id.valuation_ratio"""
