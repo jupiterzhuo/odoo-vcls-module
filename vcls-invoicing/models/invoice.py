9f878c15b17907d530d719b2a3aabb80f6df4793
Keaton <k.aylor@voisinconsulting.com>
2020-06-18 14:08:36 +0200

    007084 update bank with currency of invoice
    updated create function to get the currency of the invoice and use that corresponding bank

--- vcls-invoicing/models/invoice.py
+++ vcls-invoicing/models/invoice.py
@@ -553,6 +553,9 @@ class Invoice(models.Model):
         invoice._onchange_partner_id()
         _logger.info("INVOICE CREATE ID {} VALS {}".format(invoice.id, vals))
         invoice._message_subscribe_account_payable()
+        bank_with_currency = self.env['res.partner.bank'].search([('company_id', '=', invoice.company_id.id),('currency_id', '=', invoice.currency_id.id)],limit=1)
+        if bank_with_currency:
+            invoice.partner_bank_id = bank_with_currency
         return invoice
 
     def _message_subscribe_account_payable(self):
