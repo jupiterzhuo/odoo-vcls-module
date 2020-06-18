b5bfb4edae79af529c4987ebcc1374717011b871
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-06-18 17:40:32 +0200

    0 hours TS removed from activity rep and invoice

--- vcls-invoicing/models/invoice.py
+++ vcls-invoicing/models/invoice.py
@@ -204,7 +204,7 @@ class Invoice(models.Model):
         for parent_task, list_tasks in list_timesheet_to_compute.items():
             number_tasks = len(list_tasks)
             for task_individual in list_tasks:
-                for timesheet_id in task_individual.timesheet_ids.filtered(lambda t: t.timesheet_invoice_id.id == self.id):
+                for timesheet_id in task_individual.timesheet_ids.filtered(lambda t: t.timesheet_invoice_id.id == self.id and t.unit_amount_rounded>0):
                     if self.merge_subtask and timesheet_id.task_id.parent_id:  # if the task has a parent and we want to merge
                         current_task_id = timesheet_id.task_id.parent_id
                     else:
@@ -322,7 +322,7 @@ class Invoice(models.Model):
         self.ensure_one()
         data = OrderedDict()
         total_not_taxed = 0.
-        for timesheet_id in self.timesheet_ids.filtered(lambda t: t.so_line.qty_invoiced)\
+        for timesheet_id in self.timesheet_ids.filtered(lambda t: t.so_line.qty_invoiced and t.unit_amount_rounded>0)\
                 .sorted(lambda t: t.so_line.price_unit, reverse=True):
             rate_sale_line_id = timesheet_id.so_line
 
@@ -376,7 +376,7 @@ class Invoice(models.Model):
         self.ensure_one()
         data = OrderedDict()
         total_not_taxed = 0.
-        for timesheet_id in self.timesheet_ids.filtered(lambda t: t.so_line.qty_invoiced)\
+        for timesheet_id in self.timesheet_ids.filtered(lambda t: t.so_line.qty_invoiced and t.unit_amount_rounded>0)\
                 .sorted(lambda t: t.so_line.price_unit, reverse=True):
             rate_sale_line_id = timesheet_id.so_line
 
