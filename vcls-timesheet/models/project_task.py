36e932ec96de063eed926090599cf0eb646a6eab
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-07-02 15:45:36 +0200

    Update project_task.py

--- vcls-timesheet/models/project_task.py
+++ vcls-timesheet/models/project_task.py
@@ -380,19 +380,19 @@ class ProjectTask(models.Model):
                 .write_date
 
     @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
         if self._context.get('desc_order_display'):
             domain = list(args)
             domain.append(('last_updated_timesheet_date', '!=', False))
             new_order = 'last_updated_timesheet_date desc'
-            las_res = super(ProjectTask, self).search(domain, offset=offset, limit=limit, order=new_order,
+            las_res = super(ProjectTask, self)._search(domain, offset=offset, limit=limit, order=new_order,
                                                        count=count, access_rights_uid=access_rights_uid)
             domain = list(args)
             domain.append(('last_updated_timesheet_date', '=', False))
-            res = super(ProjectTask, self).search(domain, offset=offset, limit=limit, order=order,
+            res = super(ProjectTask, self)._search(domain, offset=offset, limit=limit, order=order,
                                                    count=count, access_rights_uid=access_rights_uid)
             return las_res + res
-        return super(ProjectTask, self).search(args, offset=offset, limit=limit, order=order,
+        return super(ProjectTask, self)._search(args, offset=offset, limit=limit, order=order,
                                                 count=count, access_rights_uid=access_rights_uid)
 
     @api.multi
