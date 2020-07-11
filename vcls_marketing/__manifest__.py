25f6c69bdc278766fe2e73626be20bcb6a2a34a6
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-07-11 23:32:31 +0200

    Update __manifest__.py

--- vcls_marketing/__manifest__.py
+++ vcls_marketing/__manifest__.py
@@ -11,7 +11,7 @@
     'website': "http://www.voisinconsulting.com",
     'category': 'Uncategorized',
 
-    'version': '0.1.23',
+    'version': '0.1.24',
   
     'depends': [
         'crm',
@@ -31,6 +31,6 @@
         'views/lead_views.xml',
         'views/partner_views.xml',
         'views/marketing_campaign_views.xml',
-        'views/dashboard_views',
+        'views/dashboard_views.xml',
     ],
 }
