9bf54b18deff794c4a3af40ee0b54e3a11547baa
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-07-11 22:33:41 +0200

    Update project_task.py

--- vcls-timesheet/models/project_task.py
+++ vcls-timesheet/models/project_task.py
@@ -378,9 +378,10 @@ class ProjectTask(models.Model):
     def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
         #_logger.info("T SEARCH {}".format(self._context))
         if 'parent_project_id' in self._context:
-            domain = list(args)
-            for item in domain:
-                _logger.info("T SEARCH DOMAIN PART {}".format(item[0]))
+            #domain = list(args)
+            _logger.info("T SEARCH DOMAIN {}".format(domain))
+            domain = list(filter(lambda d: d[0]!='project_id',list(args)))
+            _logger.info("T SEARCH DOMAIN F {}".format(domain))
             if self._context.get('parent_project_id'):
                 parent = self.env['project.project'].browse(self._context.get('parent_project_id'))
                 projects = parent | parent.child_id
