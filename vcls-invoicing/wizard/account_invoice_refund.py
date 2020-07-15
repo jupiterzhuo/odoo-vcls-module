f77c3465f8bb8bf90dfb393de4632d552477a4c5
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-07-15 08:39:19 +0200

    Update account_invoice_refund.py

--- vcls-invoicing/wizard/account_invoice_refund.py
+++ vcls-invoicing/wizard/account_invoice_refund.py
@@ -24,7 +24,6 @@ class AccountInvoiceRefund(models.TransientModel):
     def compute_refund(self, mode='refund'):
         to_process = self.env['account.invoice'].browse(self._context.get('active_ids',False))
         ret = super().compute_refund(mode)
-        to_process.mapped('refund_invoice_ids').write({'period_start':to_process.period_start,'timesheet_limit_date':to_process.timesheet_limit_date})
         if mode in ('cancel', 'modify'):
             #if cancel or modify, we release the potential timesheets and add the invoice ref to the so_line for a proper invoiced_qty calculation
             sale_lines = self.env['sale.order.line']
@@ -32,6 +31,7 @@ class AccountInvoiceRefund(models.TransientModel):
                 _logger.info("INVOICE TO REFUND {}".format(inv.number))
                 inv.release_timesheets()
                 rinv = self.env['account.invoice'].search([('type','=','out_refund'),('origin','=',inv.number)],limit=1)
+                rinv.write({'period_start':inv.period_start,'timesheet_limit_date':inv.timesheet_limit_date})
                 if rinv:
                     _logger.info("Found Credit note {} for invoice {}".format(rinv.number,inv.number))
                     for inv_line in inv.invoice_line_ids.filtered(lambda l: l.sale_line_ids and l.product_id):
