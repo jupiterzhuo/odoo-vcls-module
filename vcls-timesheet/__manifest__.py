c0bd772467e9a53149fbf146db3cf7799afb944d
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-07-28 07:59:42 +0200

    TS view update

--- vcls-timesheet/__manifest__.py
+++ vcls-timesheet/__manifest__.py
@@ -16,7 +16,7 @@
     # for the full list
     'category': 'Uncategorized',
 
-    'version': '0.8.56',
+    'version': '0.8.57',
   
     # any module necessary for this one to work correctly
     'depends': [
