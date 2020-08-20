4c86aa9b55f26a09d9a900e46dc672b13c8c1f75
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-08-20 08:06:40 +0200

    log

--- vcls-timesheet/models/sale.py
+++ vcls-timesheet/models/sale.py
@@ -119,8 +119,10 @@ class SaleOrder(models.Model):
     
     def action_compute_kpi(self):
         sos = self.browse(self.env.context.get('active_ids'))
+        
         sos.mapped('tasks_ids')._get_kpi()
         sos.mapped('project_ids')._get_kpi()
+        _logger.info("KPI RECOMPUTE {} \n{} \n{}".format(sos.mapped('name'),sos.mapped('tasks_ids.name'),sos.mapped('project_ids.name')))
 
     @api.multi
     def recompute_lines(self):
