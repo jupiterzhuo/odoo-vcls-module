af0c313cabee921f1f5277bd73ce05a1d16b2e9c
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-09-02 14:54:45 +0200

    Company as VCLS entity in contacts

--- vcls-suppliers/__manifest__.py
+++ vcls-suppliers/__manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'Uncategorized',
 
-    'version': '0.2.18',
+    'version': '0.2.19',
 
     # any module necessary for this one to work correctly
     'depends': [
