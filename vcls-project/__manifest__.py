47a3d693989c164ab21629fc72b7a66b60851491
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-09-07 15:59:03 +0200

    FIX

--- vcls-project/__manifest__.py
+++ vcls-project/__manifest__.py
@@ -16,7 +16,7 @@
     # for the full list
     'category': 'Uncategorized',
 
-    'version': '1.0.40',
+    'version': '1.0.41',
 
     # any module necessary for this one to work correctly
     'depends': [
